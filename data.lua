467b20af29db0c8afec8894cb3a7446307e5eef2
Chris Richards <doktorstick@gmail.com>
2020-05-09 01:44:10 -0500

    More better performance

--- data.lua
+++ data.lua
@@ -1,15 +1,248 @@
 local util = require "util"
 
+local fireutil = {}
+
+
+function fireutil.foreach(table_, fun_)
+  for k, tab in pairs(table_) do
+    fun_(tab)
+    if tab.hr_version then
+      fun_(tab.hr_version)
+    end
+  end
+  return table_
+end
+
+
+function fireutil.create_fire_pictures(opts)
+  local fire_blend_mode = opts.blend_mode or "additive"
+  local fire_animation_speed = opts.animation_speed or 0.5
+  local fire_scale = opts.scale or 1
+  local fire_tint = opts.tint or {r=1,g=1,b=1,a=1}
+  local fire_flags = { "compressed" }
+  local retval =
+  {
+    {
+      filename = "__base__/graphics/entity/fire-flame/fire-flame-13.png",
+      line_length = 8,
+      width = 60,
+      height = 118,
+      frame_count = 25,
+      axially_symmetrical = false,
+      direction_count = 1,
+      blend_mode = fire_blend_mode,
+      animation_speed = fire_animation_speed,
+      scale = fire_scale,
+      tint = fire_tint,
+      flags = fire_flags,
+      shift = { -0.0390625, -0.90625 }
+    },
+    {
+      filename = "__base__/graphics/entity/fire-flame/fire-flame-12.png",
+      line_length = 8,
+      width = 63,
+      height = 116,
+      frame_count = 25,
+      axially_symmetrical = false,
+      direction_count = 1,
+      blend_mode = fire_blend_mode,
+      animation_speed = fire_animation_speed,
+      scale = fire_scale,
+      tint = fire_tint,
+      flags = fire_flags,
+      shift = { -0.015625, -0.914065 }
+    },
+    {
+      filename = "__base__/graphics/entity/fire-flame/fire-flame-11.png",
+      line_length = 8,
+      width = 61,
+      height = 122,
+      frame_count = 25,
+      axially_symmetrical = false,
+      direction_count = 1,
+      blend_mode = fire_blend_mode,
+      animation_speed = fire_animation_speed,
+      scale = fire_scale,
+      tint = fire_tint,
+      flags = fire_flags,
+      shift = { -0.0078125, -0.90625 }
+    },
+    {
+      filename = "__base__/graphics/entity/fire-flame/fire-flame-10.png",
+      line_length = 8,
+      width = 65,
+      height = 108,
+      frame_count = 25,
+      axially_symmetrical = false,
+      direction_count = 1,
+      blend_mode = fire_blend_mode,
+      animation_speed = fire_animation_speed,
+      scale = fire_scale,
+      tint = fire_tint,
+      flags = fire_flags,
+      shift = { -0.0625, -0.64844 }
+    },
+    {
+      filename = "__base__/graphics/entity/fire-flame/fire-flame-09.png",
+      line_length = 8,
+      width = 64,
+      height = 101,
+      frame_count = 25,
+      axially_symmetrical = false,
+      direction_count = 1,
+      blend_mode = fire_blend_mode,
+      animation_speed = fire_animation_speed,
+      scale = fire_scale,
+      tint = fire_tint,
+      flags = fire_flags,
+      shift = { -0.03125, -0.695315 }
+    },
+    {
+      filename = "__base__/graphics/entity/fire-flame/fire-flame-08.png",
+      line_length = 8,
+      width = 50,
+      height = 98,
+      frame_count = 32,
+      axially_symmetrical = false,
+      direction_count = 1,
+      blend_mode = fire_blend_mode,
+      animation_speed = fire_animation_speed,
+      scale = fire_scale,
+      tint = fire_tint,
+      flags = fire_flags,
+      shift = { -0.0546875, -0.77344 }
+    },
+    {
+      filename = "__base__/graphics/entity/fire-flame/fire-flame-07.png",
+      line_length = 8,
+      width = 54,
+      height = 84,
+      frame_count = 32,
+      axially_symmetrical = false,
+      direction_count = 1,
+      blend_mode = fire_blend_mode,
+      animation_speed = fire_animation_speed,
+      scale = fire_scale,
+      tint = fire_tint,
+      flags = fire_flags,
+      shift = { 0.015625, -0.640625 }
+    },
+    {
+      filename = "__base__/graphics/entity/fire-flame/fire-flame-06.png",
+      line_length = 8,
+      width = 65,
+      height = 92,
+      frame_count = 32,
+      axially_symmetrical = false,
+      direction_count = 1,
+      blend_mode = fire_blend_mode,
+      animation_speed = fire_animation_speed,
+      scale = fire_scale,
+      tint = fire_tint,
+      flags = fire_flags,
+      shift = { 0, -0.83594 }
+    },
+    {
+      filename = "__base__/graphics/entity/fire-flame/fire-flame-05.png",
+      line_length = 8,
+      width = 59,
+      height = 103,
+      frame_count = 32,
+      axially_symmetrical = false,
+      direction_count = 1,
+      blend_mode = fire_blend_mode,
+      animation_speed = fire_animation_speed,
+      scale = fire_scale,
+      tint = fire_tint,
+      flags = fire_flags,
+      shift = { 0.03125, -0.882815 }
+    },
+    {
+      filename = "__base__/graphics/entity/fire-flame/fire-flame-04.png",
+      line_length = 8,
+      width = 67,
+      height = 130,
+      frame_count = 32,
+      axially_symmetrical = false,
+      direction_count = 1,
+      blend_mode = fire_blend_mode,
+      animation_speed = fire_animation_speed,
+      scale = fire_scale,
+      tint = fire_tint,
+      flags = fire_flags,
+      shift = { 0.015625, -1.109375 }
+    },
+    {
+      filename = "__base__/graphics/entity/fire-flame/fire-flame-03.png",
+      line_length = 8,
+      width = 74,
+      height = 117,
+      frame_count = 32,
+      axially_symmetrical = false,
+      direction_count = 1,
+      blend_mode = fire_blend_mode,
+      animation_speed = fire_animation_speed,
+      scale = fire_scale,
+      tint = fire_tint,
+      flags = fire_flags,
+      shift = { 0.046875, -0.984375 }
+    },
+    {
+      filename = "__base__/graphics/entity/fire-flame/fire-flame-02.png",
+      line_length = 8,
+      width = 74,
+      height = 114,
+      frame_count = 32,
+      axially_symmetrical = false,
+      direction_count = 1,
+      blend_mode = fire_blend_mode,
+      animation_speed = fire_animation_speed,
+      scale = fire_scale,
+      tint = fire_tint,
+      flags = fire_flags,
+      shift = { 0.0078125, -0.96875 }
+    },
+    {
+      filename = "__base__/graphics/entity/fire-flame/fire-flame-01.png",
+      line_length = 8,
+      width = 66,
+      height = 119,
+      frame_count = 32,
+      axially_symmetrical = false,
+      direction_count = 1,
+      blend_mode = fire_blend_mode,
+      animation_speed = fire_animation_speed,
+      scale = fire_scale,
+      tint = fire_tint,
+      flags = fire_flags,
+      shift = { -0.0703125, -1.039065 }
+    }
+  }
+  return fireutil.foreach(retval, function(tab)
+    if tab.shift and tab.scale then tab.shift = { tab.shift[1] * tab.scale, tab.shift[2] * tab.scale } end
+  end)
+end
+
+
 -- By extending the `initial_flame_count` and `delay_between_initial_flames`,
 -- the flames and its smoke can be extended for long periods, almost like
 -- little brush fires are re-igniting.
+
 
 local flame_0 = util.table.deepcopy (data.raw.fire["fire-flame"])
 flame_0.name = "creeper-flame-0"
 flame_0.initial_lifetime = 480  -- default: 120
 flame_0.delay_between_initial_flames = 240 -- default: 10
+flame_0.light.intensity = 0.5  -- default: 1
 flame_0.light.size = 10  -- default: 20
 flame_0.working_sound.match_volume_to_activity = false
+flame_0.pictures = fireutil.create_fire_pictures({
+  blend_mode = "additive",
+  animation_speed = 0.5,
+  scale = 0.4,
+  tint = { r = 1, g = 0.6, b = 1, a = 1 }
+})
+
 
 local flame_1 = util.table.deepcopy (flame_0)
 flame_1.name = "creeper-flame-1"
--- fire.lua
+++ fire.lua
@@ -3,22 +3,37 @@
 
 local fire = {
     FLAME_TICKS = 19,
-    FLAMES_PER_TICK = 31,
     -- XXX: These need to be programmatic based on the creeper flames
     -- XXX: and vice versa.
     SPREAD_DELAY = { 300, 180 },
-    SPREAD_N_DELAY = { 480, 480 },
-    SPREAD_LIMIT = { 100, 1000 }
+    SPREAD_N_DELAY = { 240, 240 },
+    SPREAD_LIMIT = { 100, 1000 },
+
+    creeper_flames = {
+        [0] = "creeper-flame-0",
+        [1] = "creeper-flame-1"
+    }
 }
 
 
 local util = require "util"
 local table_deepcopy = util.table.deepcopy
 
-print = function(...) end
+table.insert_keyed_list = function (tbl, key, value)
+    if tbl[key] == nil then
+        tbl[key] = { value }
+    else
+        table.insert (tbl[key], value)
+    end
+end
+
+
+--print = function(...) end
+
 
 function fire.on_init()
     global.active_flames = {}
+    global.by_tick = {}
 
     -- Indexed by the seed flame index. Used to track how many more
     -- flames can be spawned by it and its descendants.
@@ -91,29 +106,39 @@ function fire.on_trigger_created_entity (event)
             { name = "landfill", position = tile.position }
         })
 
+        local force = flame.force
+        flame.destroy()
+
+        -- Replace it with ours since it is on creep.
+        local entity = surface.create_entity {
+            name = fire.creeper_flames[0],
+            position = tile.position,
+            force = force
+        }
+        flame = FlameEntity (entity)
+
         __debug()  -- XXX: CONFIG
 
-        global.active_flames[flame.index] = {
+        local flame_info = {
             create_tick = event.tick,
             flame = flame,
             seed_index = flame.index,
             spawned_flames = 0,
             spread_tick = when_spread (event.tick)
         }
+        global.active_flames[flame.index] = flame_info
+        table.insert_keyed_list (
+                global.by_tick, flame_info.spread_tick, flame.index
+        )
         global.spread_limits[flame.index] = math.random (
                 fire.SPREAD_LIMIT[1], fire.SPREAD_LIMIT[2]
         )
-        print (event.tick, "active_flames", serpent.line (global.active_flames))
-        print (event.tick, "spread_limits", serpent.line (global.spread_limits))
     end
 end
 
 
 function spread_flame (tick, flame_info, flame_count)
     -- Bind locally.
-    local active_flames = global.active_flames
-    local spread_limits = global.spread_limits
-
     local math_random = math.random
     local table_insert = table.insert
 
@@ -123,26 +148,25 @@ function spread_flame (tick, flame_info, flame_count)
     local surrounding_creep = surface.find_tiles_filtered {
         name = "kr-creep",
         position = flame_info.flame.position,
-        radius = 2
+        radius = 2.5
     }
 
     local found_creep = table_size (surrounding_creep)
     local seed_index = flame_info.seed_index
     flame_count = math.min (found_creep, flame_count)
 
-    local creeper_flames = {
-        [0] = "creeper-flame-0",
-        [1] = "creeper-flame-1"
-    }
-
     local new_flames = {}
     local random_offset = math_random (0, found_creep)
 
     while flame_count > 0 do
         local tile_index = (flame_count + random_offset) % found_creep + 1
+        local position = surrounding_creep[tile_index].position
+        position.x = position.x + math_random() / 2
+        position.y = position.y + math_random() / 2
+
         local entity = surface_create_entity {
-            name = creeper_flames[math_random (0, 1)],
-            position = surrounding_creep[tile_index].position,
+            name = fire.creeper_flames[math_random (0, 1)],
+            position = position,
             force = flame_info.flame.force
         }
 
@@ -169,55 +193,67 @@ end
 
 
 function fire.spread_flames (event)
+    -- TODO: Less dense flames. A flame is considered to occupy all
+    --  squares around it. So spreading skips two squares.
+    -- TODO: Figure out an sorted list to search based off the
+    --  `spread_tick` time so we don't waste time.
+
     -- TODO: Allows the spread to trees and vice-versa
     -- TODO: Wind speeds up spreading to direction
     -- TODO: Creep health by evolution
-    -- TODO: Remove decoratives from burning, and slowly have them restored
+    -- TODO: Slowly restore decoratives
     __debug()  -- XXX: @CONFIG
 
     -- Bind locally.
     local active_flames = global.active_flames
+    local by_tick = global.by_tick
     local spread_limits = global.spread_limits
-    local math_random = math.random
-
-    if table_size(active_flames) > 0 then
-        print (event.tick, "A) active_flames", serpent.line (global.active_flames))
-    end
-    if table_size (spread_limits) > 0 then
-        print (event.tick, "A) spread_limits", serpent.line (global.spread_limits))
-    end
 
     -- Table that contains tables of new flames that were generated.
     local new_flames = {}
 
-    local processed = 0
+    if table_size(active_flames) > 0 or table_size(by_tick) > 0 or table_size (spread_limits) > 0 then
+        print (event.tick, table_size(active_flames), table_size(by_tick), table_size (spread_limits))
+    end
+
     local tick = event.tick
-    for index, flame_info in pairs (active_flames) do
-        local spread_limit = spread_limits[flame_info.seed_index]
-
-        if not spread_limit or spread_limit <= 0 then
-            active_flames[index] = nil
-            spread_limits[flame_info.seed_index] = nil
-        elseif tick > flame_info.spread_tick then
-            print (event.tick, "spreading")
-            local flames, remaining_tiles = spread_flame (tick, flame_info, 1)
-
-            if flames then
-                table.insert (new_flames, flames)
-                spread_limits[flame_info.seed_index] = spread_limit - 1
-            end
-
-            if remaining_tiles == 0 then
-                -- No need to monitor this. We still need the
-                -- `spread_limits` if this was the original flame.
-                active_flames[index] = nil
-            end
-
+    for t = tick - fire.FLAME_TICKS + 1, tick do
+        local now_flame_indices = by_tick[t] or {}
+        for k, index in pairs (now_flame_indices) do
             processed = processed + 1
-            if processed > fire.FLAMES_PER_TICK then
-                break
+            local flame_info = active_flames[index]
+            if not flame_info then goto continue end
+
+            local spread_limit = spread_limits[flame_info.seed_index]
+            if not spread_limit or spread_limit <= 0 then
+                active_flames[index] = nil
+                spread_limits[flame_info.seed_index] = nil
+            elseif not flame_info.flame.valid then
+                active_flames[index] = nil
+            else
+                local flames, remaining_tiles = spread_flame (tick, flame_info, 1)
+                flame_info.spread_tick = when_spread (tick, true)
+                table.insert_keyed_list (
+                        by_tick,
+                        flame_info.spread_tick,
+                        flame_info.flame.index
+                )
+
+                if flames then
+                    table.insert (new_flames, flames)
+                    spread_limits[flame_info.seed_index] = spread_limit - 1
+                end
+
+                if remaining_tiles == 0 then
+                    -- No need to monitor this. We still need the
+                    -- `spread_limits` if this was the original flame.
+                    active_flames[index] = nil
+                end
             end
+            ::continue::
         end
+        -- All of these for this tick has been processed.
+        by_tick[t] = nil
     end
 
     -- Finalize all the spread flames, killing off the creep, and
@@ -231,24 +267,22 @@ function fire.spread_flames (event)
             local surface_index = flame.surface.index
             local landfill = { name = "landfill", position = flame.position }
 
-            if landfill_by_surface[surface_index] then
-                table.insert (landfill_by_surface[surface_index], landfill)
-            else
-                landfill_by_surface[surface_index] = { landfill }
-            end
+            table.insert_keyed_list (
+                    landfill_by_surface,
+                    surface_index,
+                    landfill
+            )
 
             active_flames[flame.index] = flame_info
+            table.insert_keyed_list (
+                    by_tick, flame_info.spread_tick, flame.index
+            )
         end
     end
 
     -- Nothing left to monitor.
     if table_size (active_flames) == 0 then
-        spread_limits = {}
-    end
-
-    if table_size(new_flames) > 0 then
-        print (event.tick, "B) active_flames", serpent.line (global.active_flames))
-        print (event.tick, "B) spread_limits", serpent.line (global.spread_limits))
+        global.spread_limits = {}
     end
 
     for surface_index, landscape_tiles in pairs (landfill_by_surface) do
